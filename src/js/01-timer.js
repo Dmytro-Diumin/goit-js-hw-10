d1dd24284bb5f6c01b168140d1411ca2c96f00d6
Dmytro-Diumin <microb201196@gmail.com>
2024-02-06 23:48:29 +0200

    added 01-timer fix 02-snackbar.css

--- src/js/01-timer.js
+++ src/js/01-timer.js
@@ -3,6 +3,13 @@ import "flatpickr/dist/flatpickr.min.css";
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
+const datetimePickerRef = document.querySelector("#datetime-picker");
+const btnStartRef = document.querySelector("[data-start]");
+const daysRef = document.querySelector('[data-days]');
+const hoursRef = document.querySelector('[data-hours]');
+const minutesRef = document.querySelector('[data-minutes]');
+const secondsRef = document.querySelector('[data-seconds]');
+let userSelectedDate;
 
 const options = {
     enableTime: true,
@@ -10,34 +17,54 @@ const options = {
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-      console.log(selectedDates[0]);
+      userSelectedDate = selectedDates[0];
+      if (userSelectedDate < Date.now()) {
+        iziToast.error({
+          message: 'Please choose a date in the future',
+          position: 'topRight',
+        });
+        btnStartRef.disabled = true;
+      } else {
+        btnStartRef.disabled = false;
+      };
     },
   };
 
-//   let userSelectedDate = onClose()
-//   if(){
-//     window.alert("Please choose a date in the future");
-//   }
+  flatpickr(datetimePickerRef, options);
+
+  btnStartRef.addEventListener("click", btnStartClick);
+  btnStartRef.disabled = true;
+
+  function btnStartClick() {
+    btnStartRef.disabled = true;
+    datetimePickerRef.disabled = true;
+    const intervalId = setInterval(() => {
+        if (userSelectedDate - new Date() >= 0) {
+            const timerDate = convertMs(userSelectedDate - new Date());
+            daysRef.textContent = addLeadingZero(timerDate.days);
+            hoursRef.textContent = addLeadingZero(timerDate.hours);
+            minutesRef.textContent = addLeadingZero(timerDate.minutes);
+            secondsRef.textContent = addLeadingZero(timerDate.seconds);
+        } else {
+            clearInterval(intervalId);
+            datetimePickerRef.disabled = false;
+        }
+    }, 1000);
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, "0");
+}
 
   function convertMs(ms) {
-    // Number of milliseconds per unit of time
     const second = 1000;
     const minute = second * 60;
     const hour = minute * 60;
     const day = hour * 24;
-  
-    // Remaining days
     const days = Math.floor(ms / day);
-    // Remaining hours
     const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
     const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
     const seconds = Math.floor((((ms % day) % hour) % minute) / second);
   
     return { days, hours, minutes, seconds };
   }
-  
-  console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-  console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-  console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
